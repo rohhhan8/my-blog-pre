2af1636ca117b589404ffb479d070194e4e78adb
Rohan M <rohanmahato7197@gmail.com>
2025-05-24 17:28:42 +0530

    Fix Vercel deployment configuration

---
+++ client/dist/assets/Spinner-1fd68904.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-0685d0f1.js";const e=()=>r.jsx("div",{className:"h-5 w-5 border-2 border-t-transparent border-gray-600 dark:border-gray-300 rounded-full animate-spin"});export{e as S};
